88598a62276a29b0dec455b00264db9eadccfb4d
IanRMC <98826018+IanRMC@users.noreply.github.com>
2024-09-16 20:41:52 -0600

    Add files via upload

---
+++ polls/admin.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timezone
+from django.contrib import admin
+
+from .models import Choice, Question
+
+list_filter = ["pub_date"]
+search_fields = ["question_text"]
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date"]
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+from django.contrib import admin
+
+admin.site.register(Question, QuestionAdmin)
